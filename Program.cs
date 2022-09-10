dd7e27b16df3f94f0def919c09f6f8d3342c795c
Anatoliy <morfik.russia@gmail.com>
2022-09-10 12:02:57 +0800

    Добавлена программа

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Здравствуйте");
+Console.WriteLine("Напишите слова через пробел");
+string text = Console.ReadLine();
+string[] array = text.Split(" ").ToArray();
+Console.WriteLine("[{0}]", string.Join(", ", array));
+string[] NewArray = new string[array.Length];
+int count = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= 3) 
+    {
+        NewArray[count] = array[i];
+        count++;
+    }
+}
+NewArray = NewArray.Where(x => x != null).ToArray();
+Console.WriteLine("[{0}]", string.Join(", ", NewArray));
